ca5f24eb38df30ffe2c774465c2cea2b0d7ef1da
RiaJul Pro <riajulpro@outlook.com>
2024-05-01 17:02:34 +0600

    Schedule validation is included into the team section

--- src/helpers/valid/validTeam.ts
+++ src/helpers/valid/validTeam.ts
@@ -5,7 +5,7 @@ export const validateTeam = [
   check("name", "Name is required").notEmpty(),
   check("profession", "Profession is required").notEmpty(),
   check("hourly_rate", "Hourly rate is required").isNumeric().notEmpty(),
-  // check("schedule", "Schedule is required")
-  //   .isArray({ min: 1 })
-  //   .withMessage("Schedule must contain at least one item"),
+  check("schedule", "Schedule is required")
+    .isArray({ min: 1 })
+    .withMessage("Schedule must contain at least one item"),
 ];
