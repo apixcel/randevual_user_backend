37ca7b9cf7f325418c0b5895cfeef86d738b544f
RiaJul Pro <riajulpro@outlook.com>
2024-05-01 14:59:39 +0600

    Adding team validate and importing validate files in routes index and adding as middleware

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -13,20 +13,28 @@ import contact from "./contact.route";
 import support from "./support.route";
 import clientReview from "./client.review.route";
 
+// Importing all validate file
+import { validateLogin, validateSign } from "../../helpers/valid/validAuth";
+import { validateShop } from "../../helpers/valid/validShop";
+import { validatePayment } from "../../helpers/valid/validPayment";
+import { validateBooking } from "../../helpers/valid/validBooking";
+import { validateService } from "../../helpers/valid/validService";
+import { validateCategory } from "../../helpers/valid/validCategory";
+import { validateTeam } from "../../helpers/valid/validTeam";
+import { validateReview } from "../../helpers/valid/validClient";
+import { validateSupport } from "../../helpers/valid/validSupport";
 
-router.use("/auth", auth);
-router.use("/user", user);
-router.use("/shop", shop);
-router.use("/payment", payment);
-router.use("/booking", booking);
-router.use("/service", service);
-router.use("/category", category);
-router.use("/team", team);
-router.use("/review", userReview);
+router.use("/auth", validateSign, auth);
+router.use("/user", validateLogin, user);
+router.use("/shop", validateShop, shop);
+router.use("/payment", validatePayment, payment);
+router.use("/booking", validateBooking, booking);
+router.use("/service", validateService, service);
+router.use("/category", validateCategory, category);
+router.use("/team", validateTeam, team);
+router.use("/review", validateReview, userReview);
 router.use("/contact", contact);
-router.use("/support", support);
-router.use("/clientreview", clientReview);
-
-
+router.use("/support", validateSupport, support);
+router.use("/clientreview", validateReview, clientReview);
 
 export default router;
